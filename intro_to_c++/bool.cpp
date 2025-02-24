d9f9636549e2d6ae14d651a216216492deeeccb5
georgiangeloww <georgigeorgievangelov5@gmail.com>
2025-02-24 10:53:28 +0200

    Add bool.cpp

---
+++ intro_to_c++/bool.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+
+int main(){
+    bool condition0 = false;
+    bool condition1 = true;
+    
+    std::cout << "The condition is: " << condition0 << std::endl;
+    std::cout << "The condition is: " << condition1 << std::endl;
+
+    return 0;
+
+}
