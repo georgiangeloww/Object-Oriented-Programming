7386d9ddebf3606479d0007794950888a1a1b3a3
georgiangeloww <georgigeorgievangelov5@gmail.com>
2025-02-24 09:55:47 +0200

    Changes to name

---
+++ intro_to_c++/name.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+# define MAX 1024
+
+using namespace std;
+
+int main(){
+    char name[MAX];
+
+    cout << "Enter your name: ";
+
+    cin >> name;
+
+    cout << "Your name is: " << name << endl;
+
+    return 0;
+}
